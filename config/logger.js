393b5c4e03bb2dffdfe6f53d4569a5497aa4558c
fafer <zhoucheng1007@163.com>
2015-07-17 10:52:47 +0800

    修改日志模块集成express

--- config/logger.js
+++ config/logger.js
@@ -2,40 +2,54 @@
  *  logger
  * https://www.recmh.com
  *
- * Copyright 2015-2020 fight
+ * Copyright 2015-2020 fafer
  * Released under the MIT license
  */
 var log4js = require('log4js');
 log4js.configure({
     appenders: [
         {
-            type: "console",
-            category: "console"
+            type: 'console',
+            category: 'console'
         },
         {
-            type: "file",
-            filename: "logs/common.log",
+            type: 'express',
+            filename: 'logs/express.log',
+            pattern: '_yyyy-MM-dd',
+            alwaysIncludePattern: false,
+            category: 'express'
+        },
+        {
+            type: 'file',
+            filename: 'logs/common.log',
             maxLogSize: 20480,
             backups: 3,
-            category: "common"
+            category: 'common'
         },
         {
-            type: "file",
-            filename: "logs/app.log",
+            type: 'file',
+            filename: 'logs/app.log',
             maxLogSize: 20480,
             backups: 10,
-            category: "app"
+            category: 'app'
         }
     ],
     replaceConsole:true,
     levels:{
-        console: "debug",
-        common: "debug",
-        app:"debug"
+        console: 'debug',
+        express: 'debug',
+        common: 'debug',
+        app:'debug'
     }
 });
 
 exports.getLog = function(name) {
     var logger = log4js.getLogger(name);
     return logger;
+};
+
+exports.use = function(app) {
+    //页面请求日志,用auto的话,默认级别是WARN
+    //app.use(log4js.connectLogger(dateFileLog, {level:'auto', format:':method :url'}));
+    app.use(log4js.connectLogger(exports.getLog('express'), {level:'debug', format:':method :url'}));
 };
